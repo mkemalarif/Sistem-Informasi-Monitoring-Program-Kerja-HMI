87de96d149cd6e0ebc9c39057b92f8c12f6f8d30
dipdhaz <115815419+dipdhaz@users.noreply.github.com>
2023-05-24 18:06:17 +0800

    ui
    berita
    infomation box

--- resources/views/HomePage.blade.php
+++ resources/views/HomePage.blade.php
@@ -1,72 +1,117 @@
 @extends('partial.main')
 
 @section('container')
-  <h1>Welcome to the Homepage</h1>
-  <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-bs-target="#myCarousel" data-bs-slide-to="0" class="active"></li>
-      <li data-bs-target="#myCarousel" data-bs-slide-to="1"></li>
-      <li data-bs-target="#myCarousel" data-bs-slide-to="2"></li>
-      <li data-bs-target="#myCarousel" data-bs-slide-to="3"></li>
-      <li data-bs-target="#myCarousel" data-bs-slide-to="4"></li>
-    </ol>
+  <div class="container">
+    <h1 class="mt-5">Berita</h1>
 
-    <!-- Slides -->
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="https://via.placeholder.com/500x250" class="d-block w-100" alt="News Image 1">
-        <div class="carousel-caption">
-          <h5>News Title 1</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum magna eget interdum.</p>
-          <a class="btn btn-primary" href="#myCarousel" role="button" data-bs-slide="next">Read More</a>
+    <img src="../../public/assets/kata2.png" class="d-block mx-auto mt-4" alt="Gambar di atas informasi box">
+
+    <!-- Carousel -->
+    <div id="myCarousel" class="carousel slide mt-4" data-bs-ride="carousel">
+      <!-- Slides -->
+      <div class="carousel-inner">
+        <!-- Slide 1 -->
+        <div class="carousel-item active">
+          <img src="https://via.placeholder.com/500x250" class="d-block w-100" alt="News Image 1">
+          <div class="carousel-caption">
+            <h5>News Title 1</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum magna eget interdum.</p>
+            <a class="btn btn-primary" href="#myCarousel" role="button" data-bs-slide="next">Read More</a>
+          </div>
+        </div>
+        <!-- Slide 2 -->
+        <div class="carousel-item">
+          <img src="https://via.placeholder.com/500x250" class="d-block w-100" alt="News Image 2">
+          <div class="carousel-caption">
+            <h5>News Title 2</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum magna eget interdum.</p>
+            <a class="btn btn-primary" href="#myCarousel" role="button" data-bs-slide="next">Read More</a>
+          </div>
+        </div>
+        <!-- Slide 3 -->
+        <div class="carousel-item">
+          <img src="https://via.placeholder.com/500x250" class="d-block w-100" alt="News Image 3">
+          <div class="carousel-caption">
+            <h5>News Title 3</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum magna eget interdum.</p>
+            <a class="btn btn-primary" href="#myCarousel" role="button" data-bs-slide="next">Read More</a>
+          </div>
+        </div>
+        <!-- Slide 4 -->
+        <div class="carousel-item">
+          <img src="https://via.placeholder.com/500x250" class="d-block w-100" alt="News Image 4">
+          <div class="carousel-caption">
+            <h5>News Title 4</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum magna eget interdum.</p>
+            <a class="btn btn-primary" href="#myCarousel" role="button" data-bs-slide="next">Read More</a>
+          </div>
+        </div>
+        <!-- Slide 5 -->
+        <div class="carousel-item">
+          <img src="https://via.placeholder.com/500x250" class="d-block w-100" alt="News Image 5">
+          <div class="carousel-caption">
+            <h5>News Title 5</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum magna eget interdum.</p>
+            <a class="btn btn-primary" href="#myCarousel" role="button" data-bs-slide="next">Read More</a>
+          </div>
         </div>
       </div>
-      <div class="carousel-item">
-        <img src="https://via.placeholder.com/500x250" class="d-block w-100" alt="News Image 2">
-        <div class="carousel-caption">
-          <h5>News Title 2</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum magna eget interdum.</p>
-          <a class="btn btn-primary" href="#myCarousel" role="button" data-bs-slide="next">Read More</a>
+
+      <!-- Information Boxes -->
+      <div class="container mt-4">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Badan Koordinasi (Badko)</h5>
+                <p class="card-text">6</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Cabang</h5>
+                <p class="card-text">40</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row mt-4">
+          <div class="col-md-6">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Komisariat</h5>
+                <p class="card-text">76</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Total Anggota</h5>
+                <p class="card-text">5000</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div class="carousel-item">
-        <img src="https://via.placeholder.com/500x250" class="d-block w-100" alt="News Image 3">
-        <div class="carousel-caption">
-          <h5>News Title 3</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum magna eget interdum.</p>
-          <a class="btn btn-primary" href="#myCarousel" role="button" data-bs-slide="next">Read More</a>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <img src="https://via.placeholder.com/500x250" class="d-block w-100" alt="News Image 4">
-        <div class="carousel-caption">
-          <h5>News Title 4</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum magna eget interdum.</p>
-          <a class="btn btn-primary" href="#myCarousel" role="button" data-bs-slide="next">Read More</a>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <img src="https://via.placeholder.com/500x250" class="d-block w-100" alt="News Image 5">
-        <div class="carousel-caption">
-          <h5>News Title 5</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum magna eget interdum.</p>
-          <a class="btn btn-primary" href="#myCarousel" role="button" data-bs-slide="next">Read More</a>
-        </div>
-      </div>
+
+      <!-- Controls -->
+      <a class="carousel-control-prev" href="#myCarousel" role="button" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#myCarousel" role="button" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </a>
     </div>
-
-    <!-- Controls -->
-    <a class="carousel-control-prev" href="#myCarousel" role="button" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#myCarousel" role="button" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </a>
   </div>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" crossorigin="anonymous"></script>
+  <script>
+    // Inisialisasi carousel dengan JavaScript
+    var myCarousel = document.querySelector('#myCarousel');
+    var carousel = new bootstrap.Carousel(myCarousel);
+  </script>
 @endsection
--- resources/views/partial/main.blade.php
+++ resources/views/partial/main.blade.php
@@ -11,12 +11,16 @@
   <link rel="stylesheet" href="{{ asset('assets/css/logout.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/css/registrasi.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/css/profileanggota.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/HomePage.css') }}">
+  <style>
+
+  </style>
   <title>Document</title>
 </head>
 
 <body>
- {{-- Navbar --}}
-<nav class="navbar navbar-expand-lg navbar-light bg-light shadow">
+  {{-- Navbar --}}
+  <nav class="navbar navbar-expand-lg navbar-light bg-light shadow">
     <a class="navbar-brand" href="#">HMI MAKASSAR</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
       aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -53,15 +57,18 @@
     </div>
   </nav>
 
+  {{-- Banner --}}
+  <div class="banner">
+    <!-- Konten banner -->
+  </div>
+
   {{-- Container --}}
   <div class="container">
     @yield('container')
   </div>
 
-
-
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" crossorigin="anonymous"></script>
+    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 
 </html>
