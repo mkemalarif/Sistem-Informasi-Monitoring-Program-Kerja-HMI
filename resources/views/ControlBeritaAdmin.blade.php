509995d4eeef06becb4037b74dcdf3788c71f53b
dipdhaz <115815419+dipdhaz@users.noreply.github.com>
2023-05-26 22:46:46 +0800

    view
    edit berita
    tambah berita
    ui control berita admin di buat lebih menarik

--- resources/views/ControlBeritaAdmin.blade.php
+++ resources/views/ControlBeritaAdmin.blade.php
@@ -8,45 +8,47 @@
                 <div class="card-body">
                     <h1 class="mb-4">Tabel Entry Berita</h1>
                     
-                    <table class="table table-bordered">
-                        <thead>
-                            <tr>
-                                <th>Judul</th>
-                                <th>Gambar</th>
-                                <th>Status</th>
-                                <th>Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Judul Berita 1</td>
-                                <td><img src="gambar1.jpg" alt="Gambar Berita 1"></td>
-                                <td><span class="badge badge-warning">Pending</span></td>
-                                <td>
-                                    <a href="#" class="btn btn-sm btn-success">Terima</a>
-                                    <a href="#" class="btn btn-sm btn-danger">Tolak</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Judul Berita 2</td>
-                                <td><img src="gambar2.jpg" alt="Gambar Berita 2"></td>
-                                <td><span class="badge badge-success">Accepted</span></td>
-                                <td>
-                                    <a href="#" class="btn btn-sm btn-success">Terima</a>
-                                    <a href="#" class="btn btn-sm btn-danger">Tolak</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Judul Berita 3</td>
-                                <td><img src="gambar3.jpg" alt="Gambar Berita 3"></td>
-                                <td><span class="badge badge-danger">Rejected</span></td>
-                                <td>
-                                    <a href="#" class="btn btn-sm btn-success">Terima</a>
-                                    <a href="#" class="btn btn-sm btn-danger">Tolak</a>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-hover">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th class="align-middle">Judul</th>
+                                    <th class="align-middle">Gambar</th>
+                                    <th class="align-middle">Status</th>
+                                    <th class="align-middle">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td class="align-middle">Judul Berita 1</td>
+                                    <td class="align-middle"><img src="gambar1.jpg" alt="Gambar Berita 1" class="img-thumbnail"></td>
+                                    <td class="align-middle"><span class="badge badge-warning">Pending</span></td>
+                                    <td class="align-middle">
+                                        <a href="#" class="btn btn-sm btn-success"><i class="fas fa-check"></i> Terima</a>
+                                        <a href="#" class="btn btn-sm btn-danger"><i class="fas fa-times"></i> Tolak</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="align-middle">Judul Berita 2</td>
+                                    <td class="align-middle"><img src="gambar2.jpg" alt="Gambar Berita 2" class="img-thumbnail"></td>
+                                    <td class="align-middle"><span class="badge badge-success">Accepted</span></td>
+                                    <td class="align-middle">
+                                        <a href="#" class="btn btn-sm btn-success"><i class="fas fa-check"></i> Terima</a>
+                                        <a href="#" class="btn btn-sm btn-danger"><i class="fas fa-times"></i> Tolak</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="align-middle">Judul Berita 3</td>
+                                    <td class="align-middle"><img src="gambar3.jpg" alt="Gambar Berita 3" class="img-thumbnail"></td>
+                                    <td class="align-middle"><span class="badge badge-danger">Rejected</span></td>
+                                    <td class="align-middle">
+                                        <a href="#" class="btn btn-sm btn-success"><i class="fas fa-check"></i> Terima</a>
+                                        <a href="#" class="btn btn-sm btn-danger"><i class="fas fa-times"></i> Tolak</a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
