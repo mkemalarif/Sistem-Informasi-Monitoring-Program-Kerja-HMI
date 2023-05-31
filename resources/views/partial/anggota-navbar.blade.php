46f098684d7b05b3013403fa379795033efc01e0
Muhammad Kemal Arif <113968440+mkemalarif@users.noreply.github.com>
2023-05-31 21:47:44 +0800

    revisi

--- resources/views/partial/anggota-navbar.blade.php
+++ resources/views/partial/anggota-navbar.blade.php
@@ -1,16 +1,16 @@
-@section('anggota-navbar-items')
-  @if(Auth::check() && Auth::user()->role == 'anggota')
-    <li class="nav-item me-2">
-      <a class="nav-link" href="#">Edit Profil</a>
-    </li>
-    <li class="nav-item me-2">
-      <a class="nav-link" href="#">CRUD Berita</a>
-    </li>
-    <li class="nav-item me-2">
-      <a class="nav-link" href="#">Edit Profile Anggota</a>
-    </li>
-    <li class="nav-item me-2">
-      <a class="nav-link" href="#">Data Anggota</a>
-    </li>
-  @endif
-@endsection
+{{-- @section('anggota-navbar-items') --}}
+{{-- @if(Auth::check() && Auth::user()->role == 'anggota') --}}
+<li class="nav-item me-2">
+  <a class="nav-link" href="#">Edit Profil</a>
+</li>
+<li class="nav-item me-2">
+  <a class="nav-link" href="#">CRUD Berita</a>
+</li>
+<li class="nav-item me-2">
+  <a class="nav-link" href="#">Edit Profile Anggota</a>
+</li>
+<li class="nav-item me-2">
+  <a class="nav-link" href="#">Data Anggota</a>
+</li>
+@endif
+{{-- @endsection --}}
