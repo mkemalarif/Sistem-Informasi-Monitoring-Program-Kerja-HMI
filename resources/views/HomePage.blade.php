e5ef8c86fdb5c249157f9805694b932e0b0b630e
dipdhaz <115815419+dipdhaz@users.noreply.github.com>
2023-06-19 23:25:16 +0800

    bug
    homepage, admin dashboard

--- resources/views/HomePage.blade.php
+++ resources/views/HomePage.blade.php
@@ -1,9 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+@extends('partial.main')
+@section('container')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css">
     <style>
         /* Tambahkan gaya tata letak berikut di sini */
@@ -43,9 +39,9 @@
             font-size: 18px;
         }
     </style>
-</head>
 
-<body>
+
+
     <div class="container">
         <h1 class="mt-5">Berita</h1>
 
@@ -138,6 +134,4 @@
             });
         });
     </script>
-</body>
-
-</html>
+@endsection
