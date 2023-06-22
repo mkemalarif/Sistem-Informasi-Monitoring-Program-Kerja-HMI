a61b73f7a95835fec2af723d3e54b20e62df7ae5
dipdhaz <115815419+dipdhaz@users.noreply.github.com>
2023-06-22 19:46:07 +0800

    DetailProker

---
+++ resources/views/Ketua/RekapProker.blade.php
@@ -0,0 +1,68 @@
+@extends('layout.ketua')
+@section('container')
+<table class="customTable">
+  <thead>
+    <tr>
+      <th>Header 1</th>
+      <th>Header 2</th>
+      <th>Header 3</th>
+      <th>Header 4</th>
+      <th>Header 5</th>
+      <th>Header 6</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Row 1, Cell 1</td>
+      <td>Row 1, Cell 2</td>
+      <td>Row 1, Cell 3</td>
+      <td>Row 1, Cell 4</td>
+      <td>Row 1, Cell 5</td>
+      <td>Row 1, Cell 6</td>
+    </tr>
+    <tr>
+      <td>Row 2, Cell 1</td>
+      <td>Row 2, Cell 2</td>
+      <td>Row 2, Cell 3</td>
+      <td>Row 2, Cell 4</td>
+      <td>Row 2, Cell 5</td>
+      <td>Row 2, Cell 6</td>
+    </tr>
+    <tr>
+      <td>Row 3, Cell 1</td>
+      <td>Row 3, Cell 2</td>
+      <td>Row 3, Cell 3</td>
+      <td>Row 3, Cell 4</td>
+      <td>Row 3, Cell 5</td>
+      <td>Row 3, Cell 6</td>
+    </tr>
+    <tr>
+      <td>Row 4, Cell 1</td>
+      <td>Row 4, Cell 2</td>
+      <td>Row 4, Cell 3</td>
+      <td>Row 4, Cell 4</td>
+      <td>Row 4, Cell 5</td>
+      <td>Row 4, Cell 6</td>
+    </tr>
+    <tr>
+      <td>Row 5, Cell 1</td>
+      <td>Row 5, Cell 2</td>
+      <td>Row 5, Cell 3</td>
+      <td>Row 5, Cell 4</td>
+      <td>Row 5, Cell 5</td>
+      <td>Row 5, Cell 6</td>
+    </tr>
+    <tr>
+      <td>Row 6, Cell 1</td>
+      <td>Row 6, Cell 2</td>
+      <td>Row 6, Cell 3</td>
+      <td>Row 6, Cell 4</td>
+      <td>Row 6, Cell 5</td>
+      <td>Row 6, Cell 6</td>
+    </tr>
+  </tbody>
+</table>
+<!-- Generated at CSSPortal.com -->
+
+
+@endsection
--- resources/views/Login/LoginPage.blade.php
+++ resources/views/Login/LoginPage.blade.php
@@ -14,14 +14,16 @@
                             @csrf
                             <div class="mb-3 shadow-sm">
                                 <label for="username" class="form-label">Username</label>
-                                <input type="text" id="username" name="username" class="form-control" placeholder="Enter your username" required value="{{ old('username') }}">
+                                <input type="text" id="username" name="username" class="form-control"
+                                    placeholder="Enter your username" required value="{{ old('username') }}">
                                 @error('username')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
                             </div>
                             <div class="mb-3 shadow-sm">
                                 <label for="password" class="form-label">Password</label>
-                                <input type="password" id="password" name="password" class="form-control" placeholder="Enter your password" required>
+                                <input type="password" id="password" name="password" class="form-control"
+                                    placeholder="Enter your password" required>
                                 @error('password')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
@@ -43,91 +45,94 @@
     </div>
 
 
-<style>
-    .card {
-        border-radius: 10px;
-        border: none;
-        box-shadow: 0 10px 25px rgba(0, 0, 0, 0.1);
-    }
+    <style>
+        .card {
+            border-radius: 10px;
+            border: none;
+            box-shadow: 0 10px 25px rgba(0, 0, 0, 0.1);
+        }
 
-    .card-title {
-        color: #333;
-        font-size: 32px;
-        font-weight: bold;
-        text-transform: uppercase;
-        letter-spacing: 2px;
-    }
+        .card-title {
+            color: #333;
+            font-size: 32px;
+            font-weight: bold;
+            text-transform: uppercase;
+            letter-spacing: 2px;
+        }
 
-    .form-label {
-        font-weight: bold;
-        color: #555;
-        font-size: 18px;
-        margin-bottom: 8px;
-    }
+        .form-label {
+            font-weight: bold;
+            color: #555;
+            font-size: 18px;
+            margin-bottom: 8px;
+        }
 
-    .form-control {
-        border-color: #ddd;
-        box-shadow: none;
-        transition: border-color 0.3s ease-in-out;
-        font-size: 16px;
-        padding: 12px;
-        border-radius: 8px;
-        background-color: #f7f7f7;
-        color: #333;
-    }
+        .form-control {
+            border-color: #ddd;
+            box-shadow: none;
+            transition: border-color 0.3s ease-in-out;
+            font-size: 16px;
+            padding: 12px;
+            border-radius: 8px;
+            background-color: #f7f7f7;
+            color: #333;
+        }
 
-    .form-control:focus {
-        border-color: #4CAF50;
-        box-shadow: 0 0 0 0.2rem rgba(38, 143, 255, 0.5);
-    }
+        .form-control:focus {
+            border-color: #4CAF50;
+            box-shadow: 0 0 0 0.2rem rgba(38, 143, 255, 0.5);
+        }
 
-    .btn-primary {
-        background-color: #4CAF50;
-        border-color: #007bff;
-        font-size: 18px;
-        font-weight: bold;
-        letter-spacing: 1px;
-        padding: 14px 20px;
-        transition: background-color 0.3s ease-in-out;
-        border-radius: 8px;
-        text-transform: uppercase;
-    }
+        .btn-primary {
+            background-color: #4CAF50;
+            border-color: #007bff;
+            font-size: 18px;
+            font-weight: bold;
+            letter-spacing: 1px;
+            padding: 14px 20px;
+            transition: background-color 0.3s ease-in-out;
+            border-radius: 8px;
+            text-transform: uppercase;
+        }
 
-    .btn-primary:hover {
-        background-color: #0069d9;
-        border-color: #0062cc;
-    }
+        .btn-primary:hover {
+            background-color: #0069d9;
+            border-color: #0062cc;
+        }
 
-    .btn-primary:focus {
-        box-shadow: 0 0 0 0.2rem rgba(38, 143, 255, 0.5);
-    }
+        .btn-primary:focus {
+            box-shadow: 0 0 0 0.2rem rgba(38, 143, 255, 0.5);
+        }
 
-    .card-footer {
-        color: #777;
-        font-size: 12px;
-        background-color: #f7f7f7;
-        border-top: none;
-        padding: 12px;
-    }
+        .card-footer {
+            color: #777;
+            font-size: 12px;
+            background-color: #f7f7f7;
+            border-top: none;
+            padding: 12px;
+        }
 
-    .link-forgot-password {
-        text-decoration: none;
-        color: #007bff;
-        font-size: 14px;
-        transition: color 0.3s ease-in-out;
-    }
+        .link-forgot-password {
+            text-decoration: none;
+            color: #007bff;
+            font-size: 14px;
+            transition: color 0.3s ease-in-out;
+        }
 
-    .link-forgot-password:hover {
-        color: #0056b3;
-    }
-</style>
-
-@if (session('error'))
-        <script>
-            window.onload = function() {
-                alert("{{ session('error') }}");
+        .link-forgot-password:hover {
+            color: #0056b3;
+        }
+    </style>
+      <script>
+        // Check if the error message exists in the DOM
+        document.addEventListener('DOMContentLoaded', function() {
+            var errorMessage = document.querySelector('.alert-danger');
+            if (errorMessage) {
+                // Display the error message for a few seconds
+                setTimeout(function() {
+                    errorMessage.style.display = 'none';
+                }, 3000);
             }
-        </script>
-    @endif
-
+        });
+    </script>
 @endsection
